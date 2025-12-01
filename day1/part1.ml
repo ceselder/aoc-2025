fe511b86b78fa8f3b573b8fc9aa21fe61f32c539
Celeste <celestedeschamphelaere@protonmail.com>
2025-12-01 22:56:19 +0100

    part2 ocaml

--- day1/part1.ml
+++ day1/part1.ml
@@ -11,19 +11,6 @@ with End_of_file ->
   List.rev !lines ;;
 
 let real_input = read_file "input.txt"
-(* let ex_input = "L68
-L30
-R48
-L5
-R60
-L55
-L1
-L99
-R14
-L82
-";;
-
-let list_of_inputs = String.split_on_char '\n' ex_input;; *)
 
 let filtered = List.filter(fun x -> x <> "") real_input;;
 
@@ -37,7 +24,6 @@ let f elem =
         if ((String.sub elem 0 1) = "L") then (knob := (!knob + the_number) mod 100) else (knob := (!knob - the_number) mod 100);
         if (!knob = 0) then counter := !counter + 1 else ()
   in
-    List.iter f filtered;;
-
+List.iter f filtered;;
 
 Printf.printf "%d " !counter
